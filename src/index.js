6d8c2a1fc4f4f1b1af5bd4e3fdc421282ec1072d
Jisu Lee <51789449+jzizsuuz@users.noreply.github.com>
2022-04-25 15:03:43 +0900

    study: from fetch to axios library

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import Youtube from "./service/youtube";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3/",
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY }
+});
+const youtube = new Youtube(httpClient);
+
 root.render(
   <React.StrictMode>
     <App youtube={youtube} />
